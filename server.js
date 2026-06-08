76ec2c91e16056712d3474f9a8c5e57dec2fd82a
unknown <amrutak621@gmail.com>
2026-06-08 20:53:38 +0530

    frontend add

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000", process.env.FRONTEND_URL].filter(Boolean),
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://bus-frontend-peach.vercel.app"].filter(Boolean),
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
